536762ca2e4a8664301220ccfd2ce72190446129
Aless014 <alessandrobolivard@gmail.com>
2025-06-03 19:03:40 -0500

    Imprimo ubicacion del cursor

--- pantallas/menus/start.py
+++ pantallas/menus/start.py
@@ -1,14 +1,15 @@
 from pygame.surface import Surface
+import pygame
 
 class StartMenu:
     def __init__(self):
         pass
     def manejar_eventos(self, eventos):
-        print("manejando eventos")
+        self.pos=pygame.mouse.get_pos()
+        print(self.pos[0],self.pos[1])
 
-    def rellenar_pantalla(self, ventana):
+    def rellenar_pantalla(self, ventana:Surface):
         ventana.fill((255, 255, 255))
 
     def run(self,ventana:Surface):
         self.rellenar_pantalla(ventana)
-        print("Alejandro no trabaja")
