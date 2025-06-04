ba66275ce12be29b44ac88b5984ef376c3ad6354
techatlasdev <srgoqui@gmail.com>
2025-06-03 20:16:26 -0500

    Checkpoint 5: Modificar el codigo parar que los demas lo entiendan

--- pantallas/menus/start.py
+++ pantallas/menus/start.py
@@ -4,18 +4,17 @@ from utils.evento import Evento
 class StartMenu:
     def __init__(self):
         pass
+        
     def manejar_eventos(self, eventos:list[Evento]):
         for evento in eventos:
             if evento.tipo == "click":
                 coordenadas = evento.coordenadas
                 print (f"Click en coordenadas: {coordenadas}")
-            if evento.tipo == "tecla":
-                if evento == "q":
-                    print ("Tecla 'q' presionada, saliendo del menú de inicio.")
-
 
     def rellenar_pantalla(self, ventana:Surface):
-        ventana.fill((255, 255, 255))
+        ventana.fill((100, 255, 100))
 
     def run(self,ventana:Surface):
         self.rellenar_pantalla(ventana)
+
+
