df610cdbc3810b219969d8fb476f314ae0ba3e71
Aless014 <alessandrobolivard@gmail.com>
2025-06-16 09:24:14 -0500

    cambio en el heroe

--- pantallas/escenario/escenario1.py
+++ pantallas/escenario/escenario1.py
@@ -3,30 +3,30 @@ from utils.evento import Evento
 from pygame.surface import Surface
 from ..base import PantallaBase
 from objetos.entidades.moso import Mozo
-from objetos.entidades.rectangulo import Rectangulo
+from objetos.entidades.principal import Heroe
 
 class PantallaJuego(PantallaBase):
   def __init__(self):
     self.imagen=pygame.image.load("fondo.png").convert()
-    self.rectangulo = Rectangulo(coordenadas=[100, 100], velocidad=10)
+    self.heroe = Heroe(coordenadas=[100, 100], velocidad=10)
     self.mozo1 = Mozo(coordenadas=[0, 0],ancho=40,alto=80,velocidad=1,vida=5)
 
   def manejar_eventos(self, eventos):
     for evento in eventos:
       if evento.tipo == "tecla":
         if evento == "w":
-          self.rectangulo.mover_arriba()
+          self.heroe.mover_arriba()
         elif evento=="s":
-          self.rectangulo.mover_abajo()
+          self.heroe.mover_abajo()
         elif evento=="d":
-          self.rectangulo.mover_derecha()
+          self.heroe.mover_derecha()
         elif evento=="a":
-          self.rectangulo.mover_izquierda()
+          self.heroe.mover_izquierda()
 
         
   
   def run(self, pantalla:Surface):
     pantalla.blit(self.imagen,[0,0])
-    self.rectangulo.renderizar(pantalla)
+    self.heroe.renderizar(pantalla)
     self.mozo1.mover_izquierda()
     self.mozo1.renderizar(pantalla)
